b7b542620f2f433d246e0a69a8c0d0ae859ff629
Tobias Schmidt <ts@soundcloud.com>
2016-01-25 19:47:45 -0500

    Add majority of resolver statistics

--- bind_exporter.go
+++ bind_exporter.go
@@ -19,6 +19,7 @@ import (
 
 const (
 	namespace = "bind"
+	resolver  = "resolver"
 )
 
 var (
@@ -38,15 +39,73 @@ var (
 		[]string{"name"}, nil,
 	)
 	resolverQueries = prometheus.NewDesc(
-		prometheus.BuildFQName(namespace, "", "resolver_queries_total"),
+		prometheus.BuildFQName(namespace, resolver, "queries_total"),
 		"Number of outgoing DNS queries.",
 		[]string{"view", "name"}, nil,
 	)
 	resolverQueryDuration = prometheus.NewDesc(
-		prometheus.BuildFQName(namespace, "", "resolver_query_duration_seconds"),
+		prometheus.BuildFQName(namespace, resolver, "query_duration_seconds"),
 		"Resolver query round-trip time in seconds.",
 		[]string{"view"}, nil,
 	)
+	resolverQueryErrors = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, resolver, "query_errors_total"),
+		"Number of resolver queries failed.",
+		[]string{"view", "error"}, nil,
+	)
+	resolverResponseErrors = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, resolver, "response_errors_total"),
+		"Number of resolver reponse errors received.",
+		[]string{"view", "error"}, nil,
+	)
+	resolverDNSSECSucess = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, resolver, "dnssec_validation_success_total"),
+		"Number of DNSSEC validation attempts succeeded.",
+		[]string{"view", "result"}, nil,
+	)
+	resolverMetricStats = map[string]*prometheus.Desc{
+		"Lame": prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, resolver, "response_lame_total"),
+			"Number of lame delegation responses received.",
+			[]string{"view"}, nil,
+		),
+		"EDNS0Fail": prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, resolver, "query_edns0_errors_total"),
+			"Number of EDNS(0) query errors.",
+			[]string{"view"}, nil,
+		),
+		"Mismatch": prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, resolver, "response_mismatch_total"),
+			"Number of mismatch responses received.",
+			[]string{"view"}, nil,
+		),
+		"Retry": prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, resolver, "query_retries_total"),
+			"Number of resolver query retries.",
+			[]string{"view"}, nil,
+		),
+		"Truncated": prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, resolver, "response_truncated_total"),
+			"Number of truncated responses received.",
+			[]string{"view"}, nil,
+		),
+		"ValFail": prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, resolver, "dnssec_validation_errors_total"),
+			"Number of DNSSEC validation attempt errors.",
+			[]string{"view"}, nil,
+		),
+	}
+	resolverLabelStats = map[string]*prometheus.Desc{
+		"QueryAbort":    resolverQueryErrors,
+		"QuerySockFail": resolverQueryErrors,
+		"QueryTimeout":  resolverQueryErrors,
+		"NXDOMAIN":      resolverResponseErrors,
+		"SERVFAIL":      resolverResponseErrors,
+		"FORMERR":       resolverResponseErrors,
+		"OtherError":    resolverResponseErrors,
+		"ValOk":         resolverDNSSECSucess,
+		"ValNegOk":      resolverDNSSECSucess,
+	}
 )
 
 // Exporter collects Binds stats from the given server and exports
@@ -83,8 +142,14 @@ func (e *Exporter) Describe(ch chan<- *prometheus.Desc) {
 	ch <- up
 	ch <- incomingQueries
 	ch <- incomingRequests
+	ch <- resolverDNSSECSucess
 	ch <- resolverQueries
 	ch <- resolverQueryDuration
+	ch <- resolverQueryErrors
+	ch <- resolverResponseErrors
+	for _, desc := range resolverMetricStats {
+		ch <- desc
+	}
 }
 
 // Collect fetches the stats from configured bind location and
@@ -133,6 +198,19 @@ func (e *Exporter) Collect(ch chan<- prometheus.Metric) {
 			ch <- prometheus.MustNewConstMetric(
 				resolverQueries, prometheus.CounterValue, float64(s.Counter), v.Name, s.Name,
 			)
+		}
+
+		for _, s := range v.Resstat {
+			if desc, ok := resolverMetricStats[s.Name]; ok {
+				ch <- prometheus.MustNewConstMetric(
+					desc, prometheus.CounterValue, float64(s.Counter), v.Name,
+				)
+			}
+			if desc, ok := resolverLabelStats[s.Name]; ok {
+				ch <- prometheus.MustNewConstMetric(
+					desc, prometheus.CounterValue, float64(s.Counter), v.Name, s.Name,
+				)
+			}
 		}
 
 		if buckets, count, err := histogram(v.Resstat); err == nil {
