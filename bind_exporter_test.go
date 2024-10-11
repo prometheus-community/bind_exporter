f8856d25b2f2548ca3dccc1786fd287f7fea1624
SuperQ <superq@gmail.com>
2024-10-11 18:21:08 +0200

    Update exporter-toolkit
    * Update Go to 1.23.
    * Replace go-kit/log with slog.
    * Remove obsolete codespell with misspell.
    
    Signed-off-by: SuperQ <superq@gmail.com>

--- bind_exporter_test.go
+++ bind_exporter_test.go
@@ -20,10 +20,10 @@ import (
 	"testing"
 	"time"
 
-	"github.com/go-kit/log"
 	"github.com/prometheus-community/bind_exporter/bind"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/common/expfmt"
+	"github.com/prometheus/common/promslog"
 )
 
 var (
@@ -110,7 +110,7 @@ type bindExporterTest struct {
 func (b bindExporterTest) run(t *testing.T) {
 	defer b.server.Close()
 
-	o, err := collect(NewExporter(log.NewNopLogger(), b.version, b.server.URL, time.Second, b.groups))
+	o, err := collect(NewExporter(promslog.NewNopLogger(), b.version, b.server.URL, time.Second, b.groups))
 	if err != nil {
 		t.Fatal(err)
 	}
